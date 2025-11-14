3e2fe8cd91e77daf8672f0eb0fd9c1dafe6113a8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-11-13 21:05:45 -0300

    feat: ajustando a application.properties

--- src/main/java/br/com/github/rafamiura/gs_1_ai_upskilling/controller/TemaController.java
+++ src/main/java/br/com/github/rafamiura/gs_1_ai_upskilling/controller/TemaController.java
@@ -11,8 +11,8 @@ public class TemaController {
     public Map<String, String> info() {
         return Map.of(
             "tema", "Plataformas de upskilling e reskilling baseadas em IA",
-            "membro1", "Guilherme Korus Daoud",
-            "membro2", "Rafael Jun Miura",
+            "membro 1", "Guilherme Korus Daoud",
+            "membro 2", "Rafael Jun Miura",
             "descricao", "As Plataformas de Upskilling e Reskilling baseadas em Inteligência Artificial (IA) são soluções tecnológicas projetadas para ajudar indivíduos e empresas a aprimorar ou redefinir habilidades de maneira mais eficiente e personalizada. Essas plataformas utilizam IA para oferecer uma experiência de aprendizado mais adaptativa, otimizada e focada nas necessidades específicas de cada usuário, seja no desenvolvimento de novas competências (upskilling) ou na reinvenção de habilidades para diferentes funções (reskilling)"
         );
     }
